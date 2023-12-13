f27948fc16a5dead2db4c63baedbd3485aa8d2a2
Taratorin <unknownmanty@yandex.ru>
2023-12-13 11:13:36 +0300

    add tests

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDtoCreate.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDtoCreate.java
@@ -1,15 +1,16 @@
 package ru.practicum.shareit.booking.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import ru.practicum.shareit.valid.StartBeforeEndDateValid;
 
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Min;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 @AllArgsConstructor
 @StartBeforeEndDateValid
@@ -19,4 +20,17 @@ public class BookingDtoCreate {
     private LocalDateTime end;
     @Min(1)
     private long itemId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookingDtoCreate that = (BookingDtoCreate) o;
+        return itemId == that.itemId && Objects.equals(start, that.start) && Objects.equals(end, that.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end, itemId);
+    }
 }
